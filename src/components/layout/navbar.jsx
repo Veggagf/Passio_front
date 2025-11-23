fc2ba5e3846a64e4c94963c50981a7204e699fff
Regina <regis_ros@outlook.com>
2025-11-23 00:25:59 -0600

    Se hizo la pagina de register

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,18 +1,23 @@
+import { Link } from "react-router-dom";
+import { routes } from "../../router/routes";
+
 export default function Navbar() {
   return (
-    <nav className="w-full bg-black text-white px-8 py-6 flex items-center justify-between">
-      <h1 className="text-4xl font-metrophobic font-bold tracking-wide">
-        PASSIO
-      </h1>
+    <nav className="w-full bg-black text-white px-8 py-6 flex items-center justify-between border-b border-white/20">
+      <Link to={routes.home}>
+        <h1 className="text-4xl font-metrophobic font-bold tracking-wide cursor-pointer">
+          PASSIO
+        </h1>
+      </Link>
 
       <div className="flex items-center gap-4 font-jetbrains text-lg">
-
-        <a href="#" className="px-6 py-2 border border-white rounded-full hover:bg-white hover:text-black transition">
+        <Link to={routes.login} className="px-6 py-2 border border-white rounded-full hover:bg-white hover:text-black transition">
           Login
-        </a>
-        <a href="#" className="px-6 py-2 bg-white text-black rounded-full hover:bg-gray-200 transition">
+        </Link>
+
+        <Link to={routes.register} className="px-6 py-2 bg-white text-black rounded-full hover:bg-gray-200 transition">
           Register
-        </a>
+        </Link>
       </div>
     </nav>
   );
---
+++ src/pages/auth/registerPage.jsx
@@ -0,0 +1,107 @@
+import { Link } from "react-router-dom";
+import { routes } from "../../router/routes";
+
+import Navbar from "../../components/layout/navbar";
+import Footer from "../../components/layout/footer";
+import fondoregis from "../../assets/imagenes/fondoregis.jpg";
+
+function RegisterPage() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="relative w-full h-[500px] bg-cover bg-center" style={{ backgroundImage: `url(${fondoregis})` }}>
+        <div className="absolute inset-0 bg-black/50"></div>
+
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-6">
+          <h1 className="text-white text-6xl font-light drop-shadow-lg">
+            Únete a Passio y da vida a experiencias memorables
+          </h1>
+        </div>
+      </div>
+
+      <div className="bg-black w-full py-20 text-white flex justify-start px-16">
+        <div className="w-full max-w-xl">
+          <h2 className="text-4xl font-jetbrains font-semibold mb-10 tracking-wide">
+            REGÍSTRATE
+          </h2>
+
+          <form className="grid grid-cols-1 gap-6 font-jetbrains text-base">
+            {[
+              "Nombre",
+              "A. paterno",
+              "A. materno",
+              "Usuario",
+              "Correo",
+              "Contraseña",
+              "Confirmar contraseña",
+            ].map((label, index) => (
+              <div key={index} className="flex flex-col">
+                <label className="mb-1 tracking-wide">{label}</label>
+
+                <input
+                  type={label.includes("Contraseña") ? "password" : "text"}
+                  className="w-full bg-black border border-gray-600 px-4 py-3 rounded-lg focus:outline-none focus:border-white text-base"/>
+              </div>
+            ))}
+
+            <div className="flex gap-4 mt-6">
+              <button className="bg-white text-black px-6 py-2 text-base rounded-lg hover:bg-gray-200 font-jetbrains">
+                Register
+              </button>
+
+              <Link
+                to={routes.login}
+                className="border border-white px-6 py-2 text-base rounded-lg hover:bg-white hover:text-black font-jetbrains flex items-center justify-center">
+                Login
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div className="bg-black w-full py-10 px-15 text-white">
+        <h2 className="text-6xl font-semibold">Testimonials</h2>
+        <p className="text-gray-300 py-5 text-2xl mb-12">
+          Here's what people are saying
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-7xl mx-auto">
+          <div className="bg-white text-black p-6 rounded-2xl shadow-lg">
+            <p className="text-lg mb-4">
+              “Passio hizo que la gestión de boletos fuera rapidísima. El control
+              de accesos fue impecable y no tuvimos filas largas por primera vez.
+              Totalmente recomendado.”
+            </p>
+            <h3 className="font-semibold">Usuario 1</h3>
+            <span className="text-gray-500 text-sm">Evento 1</span>
+          </div>
+
+          <div className="bg-white text-black p-6 rounded-2xl shadow-lg">
+            <p className="text-lg mb-4">
+              “Comprar mis boletos fue súper fácil y la entrada al evento fue
+              instantánea. Solo escaneé y entré. La experiencia completa se sintió
+              muy moderna.”
+            </p>
+            <h3 className="font-semibold">Usuario 2</h3>
+            <span className="text-gray-500 text-sm">Evento 2</span>
+          </div>
+
+          <div className="bg-white text-black p-6 rounded-2xl shadow-lg">
+            <p className="text-lg mb-4">
+              “Gracias a Passio aumentamos nuestras ventas anticipadas y tuvimos
+              una visibilidad mucho más clara del aforo. Definitivamente elevó la
+              calidad de nuestros eventos.”
+            </p>
+            <h3 className="font-semibold">Usuario 3</h3>
+            <span className="text-gray-500 text-sm">Evento 3</span>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default RegisterPage;
